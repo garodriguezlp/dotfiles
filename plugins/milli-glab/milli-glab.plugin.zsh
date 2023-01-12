672782e2e834a7d7cdaf9983c89ccee0ca261174
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2023-01-12 16:09:00 -0500

    Add more utils to my milli gitlab plugin

--- plugins/milli-glab/milli-glab.plugin.zsh
+++ plugins/milli-glab/milli-glab.plugin.zsh
@@ -1,3 +1,4 @@
+
 # Some utility functions for working with GitLab
 
 # --- -----------------------------------------------------------------------
@@ -6,6 +7,7 @@
 export GITLAB_HOST=https://gitlab.devops.fnbo.io
 export GITLAB_G_BACLOG_PROJECT="core-phoenix/team-transact/backlog"
 export GITLAB_G_USER="gustavo"
+export GITLAB_G_REVIEWERS="jguerrero,jmcneal,evans,mrahate,schallagulla,fernando,joaquin"
 
 # --- -----------------------------------------------------------------------
 # --- Assing issue to me and move it to in progress
@@ -39,4 +41,43 @@ fun glab-issue-assign-to-me() {
         --unlabel "status::todo" \
         --label "status::doing" \
         --assignee $GITLAB_USER
+}
+
+# --- -----------------------------------------------------------------------
+# --- Mark MR as ready for review
+# --- -----------------------------------------------------------------------
+fun glab-mr-ready-for-review() {
+
+    # Check if the MR number was provided
+    if [ -z "$1" ]; then
+        echo "Usage: glab-mr-ready-for-review <mr-number>"
+        return 1
+    fi
+
+    # Check if the MR exists
+    if ! output=$(glab mr view $1 2>/dev/null); then
+        echo "MR $1 does not exist"
+        return 1
+    fi
+
+    # Display the first 2 lines of the output
+    echo "$output" | head -n 2
+
+    # Validate if the user wants to continue
+    vared -p "Are you sure you want to mark this MR as ready for review? [y/N]" -c reply
+    if [[ ! $reply =~ ^[Yy]$ ]]; then
+        return 1
+    fi
+
+    echo "Marking MR $1 as ready for review, and adding reviewers: $GITLAB_G_REVIEWERS"
+    glab mr update $1 \
+        --reviewer $GITLAB_G_REVIEWERS \
+        --ready
+}
+
+# --- -----------------------------------------------------------------------
+# --- Issues assigned to me
+# --- -----------------------------------------------------------------------
+fun glab-issues-assigned-to-me() {
+    glab issue list --repo $GITLAB_G_BACLOG_PROJECT --assignee $GITLAB_G_USER
 }
