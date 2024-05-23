b54120808fd0f1d1d1d8c67c1475065f7e114bb4
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2024-05-23 15:10:32 -0500

    Add pill reminder and update gitconfig

--- plugins/fun/fun.plugin.zsh
+++ plugins/fun/fun.plugin.zsh
@@ -8,3 +8,12 @@ function love() {
     echo "Opening web whatsapp for number: ${LOVE_NUMBER}"
     open "https://web.whatsapp.com/send/?phone=%2B57${LOVE_NUMBER}&text&type=phone_number&app_absent=0"
 }
+
+# --- ------------------------------------------------------------------------------------------------
+# --- Love's pills reminder
+# --- ------------------------------------------------------------------------------------------------
+function pills() {
+    echo "Sending pills reminder to your love... 💊"
+    echo "Opening web whatsapp for number: ${LOVE_NUMBER}"
+    open "https://web.whatsapp.com/send/?phone=%2B57${LOVE_NUMBER}&text=Baby%2C%20don%27t%20forget%20to%20take%20your%20pills.%20Love%20you%20so%20much%21&app_absent=0"
+}
